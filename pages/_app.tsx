77f2cff0736f77170e159174d580d4193929aa3a
job25721 <pathomporn_pankaew@cmu.ac.th>
2021-02-16 15:56:17 +0700

    redux implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import 'tailwindcss/tailwind.css'
-
+import { useStore } from '../store'
+import { Provider } from 'react-redux'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState)
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
